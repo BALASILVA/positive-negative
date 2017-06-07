4237552b551fce4145993b3993006c2aebb9d749
BALASILVA <balasilvabe@gmail.com>
2017-06-07 09:44:27 +0530

    Add files via upload

---
+++ acti2.java
@@ -0,0 +1,17 @@
+package dp;
+import java.io.*;
+import java.util.scanner;
+public class acti2 {
+	public static void main(String args[])
+	{
+		int sum=0,i=0;
+		int ar[]=new int[10];
+		Scanner s=new Scanner(system.in);
+		for(i=0;i<=10;i++)
+		{
+			ar[i]=s.nextInt();
+
+		}
+	}
+
+}
